7bf86d45378862c30f8da6f7f10cad5051501e60
Seth Cagampang <seth.cagampang@osnexus.com>
2025-11-20 22:21:09 +0000

    Improved install script to support patching and full install, rollback and patch reversal included

--- install-qs-pve.sh
+++ install-qs-pve.sh
@@ -4,6 +4,7 @@
 
 DO_CHECKSUM=0
 DO_ROLLBACK=0
+DO_REVERSE_PATCH=0
 # default behavior is patch install
 FULL_COPY=0
 
@@ -21,10 +22,11 @@ usage() {
     echo ""
     echo "Usage: $0 [--checksum] [--rollback] [--help]"
     echo ""
-    echo "  --checksum   Print sha256 hashes of source and target files before and after copy"
-    echo "  --rollback   Restore files from backup (undo changes made by this script)"
-    echo "  --fullcopy   Perform a full copy install (overwrite all files)"
-    echo "  --help       Show this help message"
+    echo "  --fullcopy     Perform a full copy install (overwrite all files)"
+    echo "  --checksum     Print sha256 hashes of source and target files before and after copy"
+    echo "  --rollback     Restore files from backup (undo changes made by this script)"
+    echo "  --reversepatch Restore original files by reversing applied patches"
+    echo "  --help         Show this help message"
     echo ""
     echo "Backup/rollback system:"
     echo "  - On first run, all target files (except QuantaStorPlugin.pm) are backed up to $BACKUP_DIR."
@@ -40,6 +42,9 @@ for arg in "$@"; do
         --rollback)
             DO_ROLLBACK=1
             ;;
+        --reversepatch)
+            DO_REVERSE_PATCH=1
+            ;;
         --fullcopy)
             FULL_COPY=1
             ;;
@@ -52,9 +57,14 @@ done
 
 # Use the full current working directory as the base source directory
 BASE_SOURCE_DIR="$(pwd)/src"
+PATCHES_SOURCE_DIR="$(pwd)/patches"
 SOURCE_DIR_PERL5="$BASE_SOURCE_DIR/PVE"
 SOURCE_DIR_JS="$BASE_SOURCE_DIR/pve-manager/js"
 SOURCE_DIR_APIDOC="$BASE_SOURCE_DIR/pve-docs/api-viewer"
+
+SOURCE_DIR_PATCHES_PERL5="$PATCHES_SOURCE_DIR/PVE"
+SOURCE_DIR_PATCHES_JS="$PATCHES_SOURCE_DIR/pve-manager/js"
+SOURCE_DIR_PATCHES_APIDOC="$PATCHES_SOURCE_DIR/pve-docs/api-viewer"
 
 # Target directory
 TARGET_DIR_PERL5="/usr/share/perl5/PVE"
@@ -62,9 +72,17 @@ TARGET_DIR_JS="/usr/share/pve-manager/js"
 TARGET_DIR_APIDOC="/usr/share/pve-docs/api-viewer"
 
 # create an array of the source files and an array of the target files
+FILE_QSTOR_PLUGIN=(
+    "Storage/LunCmd/QuantaStorPlugin"
+)
+
 FILE_NAMES_PERL5=(
     "Storage/ZFSPlugin"
-    "Storage/LunCmd/QuantaStorPlugin.pm"
+    "Storage/LunCmd/QuantaStorPlugin"
+)
+
+FILE_PATCHES_PERL5=(
+    "Storage/ZFSPlugin"
 )
 
 FILE_NAMES_JS=(
@@ -79,9 +97,9 @@ FILE_NAME_APIDOC=(
 # pveversion
 # pve-manager/9.1.1/42db4a6cf33dac83 (running kernel: 6.17.2-1-pve)
 # Get the raw version string
-raw_version=$(pveversion | awk -F'/' '{print $2}' | cut -d'-' -f1)
+PVE_VERSION=$(pveversion | awk -F'/' '{print $2}' | cut -d'-' -f1)
 
-echo "PVE version detected: $raw_version"
+echo "PVE version detected: $PVE_VERSION"
 
 # Backup function
 backup_files() {
@@ -129,11 +147,10 @@ copy_files() {
     local file_names=("${!1}")
     local src_dir="$2"
     local dest_dir="$3"
-    local version="$4"
-    local suffix="$5"
+    local suffix="$4"
     for file in "${file_names[@]}"; do
         local src="$src_dir/$file"
-        local dest="$dest_dir/$file.$version$suffix"
+        local dest="$dest_dir/$file.$PVE_VERSION$suffix"
         if [[ -f "$src" ]]; then
             mkdir -p "$(dirname "$dest")"
             cp "$src" "$dest"
@@ -144,11 +161,41 @@ copy_files() {
     done
 }
 
+apply_patches() {
+    local file_names=("${!1}")
+    local src_dir="$2"
+    local dest_dir="$3"
+    local suffix="$4"
+    local src="$src_dir/$file.$PVE_VERSION$suffix"
+    local dest="$dest_dir/$file"
+    if [[ -f "$src" && -f "$dest" ]]; then
+        patch "$dest" < "$src"
+        echo "Patched: $dest with $src"
+    else
+        echo "Patch source or destination file not found: $src or $dest"
+    fi
+}
+
+reverse_patches() {
+    local file_names=("${!1}")
+    local src_dir="$2"
+    local dest_dir="$3"
+    local suffix="$4"
+    local src="$src_dir/$file.$PVE_VERSION$suffix"
+    local dest="$dest_dir/$file"
+    if [[ -f "$src" && -f "$dest" ]]; then
+        patch -R "$dest" < "$src"
+        echo "Reversed patch: $dest with $src"
+    else
+        echo "Patch source or destination file not found: $src or $dest"
+    fi
+}
+
 print_hashes() {
     local file_names=("${!1}")
     local dir="$2"
+    local suffix="$3"
     local version="$4"
-    local suffix="$5"
     for file in "${file_names[@]}"; do
         local path="$dir/$file.$version$suffix"
         if [[ -f "$path" ]]; then
@@ -160,37 +207,39 @@ print_hashes() {
 }
 
 doPatchInstall() {
+    # install the copy of QSTOR plugin only
+    copy_files FILE_QSTOR_PLUGIN[@] "$SOURCE_DIR_PERL5" "$TARGET_DIR_PERL5" ".pm"
 
+    # apply patches to other files as needed
+    apply_patches FILE_PATCHES_PERL5[@] "$SOURCE_DIR_PATCHES_PERL5" "$TARGET_DIR_PERL5" ".pm"
+}
+
+doReversePatchInstall() {
+    # reverse patches to other files as needed
+    reverse_patches FILE_PATCHES_PERL5[@] "$SOURCE_DIR_PATCHES_PERL5" "$TARGET_DIR_PERL5" ".pm"
 }
 
 doFullCopyInstall() {
     # copy files from Source to Target - perl5
-    copy_files FILE_NAMES_PERL5[@] "$SOURCE_DIR_PERL5" "$TARGET_DIR_PERL5" "$raw_version" ".pm"
-
+    copy_files FILE_NAMES_PERL5[@] "$SOURCE_DIR_PERL5" "$TARGET_DIR_PERL5" ".pm"
     # copy files from Source to Target - js
-    copy_files FILE_NAMES_JS[@] "$SOURCE_DIR_JS" "$TARGET_DIR_JS" "$raw_version" ".js"
-
+    copy_files FILE_NAMES_JS[@] "$SOURCE_DIR_JS" "$TARGET_DIR_JS" ".js"
     # copy files from Source to Target - apidoc
-    copy_files FILE_NAME_APIDOC[@] "$SOURCE_DIR_APIDOC" "$TARGET_DIR_APIDOC" "$raw_version" ".js"
+    copy_files FILE_NAME_APIDOC[@] "$SOURCE_DIR_APIDOC" "$TARGET_DIR_APIDOC" ".js"
 }
 
 doChecksum() {
     # perl5 files
-    print_hashes FILE_NAMES_PERL5[@] "$SOURCE_DIR_PERL5" "$raw_version" ".pm"
+    print_hashes FILE_NAMES_PERL5[@] "$SOURCE_DIR_PERL5" ".pm" "$PVE_VERSION"
+    print_hashes FILE_NAMES_PERL5[@] "$TARGET_DIR_PERL5" ".pm" ""
     echo "---------------------------------"
-    print_hashes FILE_NAMES_PERL5[@] "$TARGET_DIR_PERL5" "" ".pm"
-    echo "---------------------------------"
-
     # js files
-    print_hashes FILE_NAMES_JS[@] "$SOURCE_DIR_JS" "$raw_version" ".js"
+    print_hashes FILE_NAMES_JS[@] "$SOURCE_DIR_JS" ".js" "$PVE_VERSION"
+    print_hashes FILE_NAMES_JS[@] "$TARGET_DIR_JS" ".js" ""
     echo "---------------------------------"
-    print_hashes FILE_NAMES_JS[@] "$TARGET_DIR_JS" "" ".js"
-    echo "---------------------------------"
-
     # apidoc files
-    print_hashes FILE_NAME_APIDOC[@] "$SOURCE_DIR_APIDOC" "$raw_version" ".js"
-    echo "---------------------------------"
-    print_hashes FILE_NAME_APIDOC[@] "$TARGET_DIR_APIDOC" "" ".js"
+    print_hashes FILE_NAME_APIDOC[@] "$SOURCE_DIR_APIDOC" ".js" "$PVE_VERSION"
+    print_hashes FILE_NAME_APIDOC[@] "$TARGET_DIR_APIDOC" ".js" ""
     echo "---------------------------------"
 }
 
@@ -202,6 +251,11 @@ if [[ $DO_ROLLBACK -eq 1 ]]; then
     rollback_files FILE_NAMES_JS[@] "$TARGET_DIR_JS" ".js"
     rollback_files FILE_NAME_APIDOC[@] "$TARGET_DIR_APIDOC" ".js"
     echo "Rollback complete."
+    exit 0
+elif [[ $DO_REVERSE_PATCH -eq 1 ]]; then
+    echo "Reversing patches on target files..."
+    doReversePatchInstall
+    echo "Reverse patch complete."
     exit 0
 fi
 
@@ -245,7 +299,6 @@ if [[ $should_backup -eq 1 ]]; then
     backup_files FILE_NAMES_JS[@] "$TARGET_DIR_JS" ".js"
     backup_files FILE_NAME_APIDOC[@] "$TARGET_DIR_APIDOC" ".js"
     echo "Backup complete."
-    exit 0
 fi
 
 # might only need to do this on full copy installs
